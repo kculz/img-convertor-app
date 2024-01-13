996c26e8e0e49967e267dd06a57eac7a49b391fb
kculz <munyamakudzai095@gmail.com>
2024-01-13 13:21:32 +0200

    FEAT: landing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,13 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "node_modules/flowbite-react/lib/esm/**/*.js",
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    //...npm install flowbite
+    require('flowbite/plugin'),
+  ],
 }
